b4f7768741656bae7ebeeced6700ab6ed1e1078e
pumpkinsoft88 <pumpkinsoft88@gmail.com>
2023-02-22 10:03:50 +0900

    first commit

---
+++ lib/components/empty_grocery_screen.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class EmptyGroceryScreen extends StatelessWidget {
+  const EmptyGroceryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Flexible(
+          child: AspectRatio(
+              aspectRatio: 1.0,
+              child: Image.asset('assets/fooderlich_assets/empty_list.png')),
+        ),
+        Text(
+          'No Groceries',
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Text(
+          'Shopping for ingredients? Write them down!',
+          style: Theme.of(context).textTheme.bodyText2,
+        ),
+        MaterialButton(
+          onPressed: () {},
+          textColor: Colors.white,
+          color: Colors.green,
+          height: 36,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(18)),
+          child: const Text('Browse Recipes'),
+        )
+      ],
+    );
+  }
+}
---
+++ lib/screens/grocery_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+import '../components/empty_grocery_screen.dart';
+
+class GroceryScreen extends StatelessWidget {
+  const GroceryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const EmptyGroceryScreen();
+  }
+}
